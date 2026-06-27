65ac43bb764e01879db571fb4c78ab9e8c67c546
Sdanielhj28 <3030687440108@ingenieria.usac.edu.gt>
2026-06-26 19:57:14 -0600

    Avance inicial proyecto 2 con Swing

---
+++ src/modelo/Investigador.java
@@ -0,0 +1,59 @@
+package modelo;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author danie
+ */
+public class Investigador implements Serializable {
+    private String codigo;
+    private String nombre;
+    private String genero;
+    private String password;
+    private int experimentos;
+
+    public Investigador(String codigo, String nombre, String genero, String password) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.genero = genero;
+        this.password = password;
+        this.experimentos = 0;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getExperimentos() {
+        return experimentos;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setExperimentos(int experimentos) {
+        this.experimentos = experimentos;
+    }
+}
---
+++ src/modelo/SistemaDatos.java
@@ -0,0 +1,11 @@
+package modelo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author danie
+ */
+public class SistemaDatos {
+    public static ArrayList<Investigador> investigadores = new ArrayList<>();
+}
