6fa77f644df0ec27fea6c85cca0827612ba81128
José Roberto Orozco <3304741891202@ingenieria.usac.edu.gt>
2026-06-28 14:57:11 -0600

    implementar módulo de patrones - modelo, crear y carga CSV

--- src/modelo/Patron.java
+++ src/modelo/Patron.java
@@ -1,37 +1,24 @@
 package modelo;
 
-/**
- *
- * @author danie
- */
-public class Patron {
+import java.io.Serializable;
+
+public class Patron implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private String codigo;
     private String nombre;
-    private String tipo;
+    private int[][] matriz;
 
-    public Patron(String codigo, String nombre, String tipo) {
+    public Patron(String codigo, String nombre, int[][] matriz) {
         this.codigo = codigo;
         this.nombre = nombre;
-        this.tipo = tipo;
+        this.matriz = matriz;
     }
 
-    public String getCodigo() {
-        return codigo;
-    }
+    public String getCodigo() { return codigo; }
+    public String getNombre() { return nombre; }
+    public int[][] getMatriz() { return matriz; }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setMatriz(int[][] matriz) { this.matriz = matriz; }
 }
